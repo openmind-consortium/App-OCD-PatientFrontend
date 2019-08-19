33c1328b82d4aba2bd6e2aaa8d2673a99510c1bc
fernandogelin <fernandogelin@gmail.com>
2019-08-19 06:24:13 -0400

    style(rec): add conditional class to recording component

--- app/components/rec.js
+++ app/components/rec.js
@@ -1,5 +1,15 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
-  status: "recording"
+  status: computed('active', function() {
+    let statusClass;
+    if (this.active){
+      statusClass = 'pulse'
+    }
+    else {
+      statusClass = 'idle'
+    }
+    return statusClass
+  })
 });
