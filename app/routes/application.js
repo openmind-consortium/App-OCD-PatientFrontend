485b44017f66c29c42269021c488e254802ad4cc
Fernando Gelin <fernando@fernandogelin.com>
2019-10-01 11:17:29 -0400

    feat(device status): fetch device status on 20 second intervals
    still throws error on updating device during reload loop - probably need to improve concurrency

--- app/routes/application.js
+++ app/routes/application.js
@@ -7,6 +7,37 @@ import { inject as service } from '@ember/service';
 export default Route.extend(I18nMixin, {
   settings: storageFor('settings'),
   zmq: service('zmq-client'),
+  store: service(),
+  afterModel(model) {
+    const getStatus = () => {
+      var device = model.store.findRecord('device', 1)
+      console.log("getting battery status")
+      const batteryMessage = {message_type: 'get', message: 'battery'}
+      const batteryStatus = this.zmq.request(JSON.stringify(batteryMessage))
+      batteryStatus
+        .then((response) => {
+          device.set('battery', response['payload']['battery_level']/100)
+        })
+        .then(() => {
+          console.log("getting stim status")
+          const stimMessage = {message_type: 'get', message: 'stim-status'}
+          const stimStatus = this.zmq.request(JSON.stringify(stimMessage))
+          stimStatus.then((response) => {
+            device.set('stimulation_voltage', response['payload']['stim_on'])
+          })
+          .then(() => {
+            console.log("getting sense status")
+            const senseMessage = {message_type: 'get', message: 'sense-status'}
+            const senseStatus = this.zmq.request(JSON.stringify(senseMessage))
+            senseStatus.then((response) => {
+              device.set('recording', response['payload']['sense_on'])
+            })
+          })
+        })
+    }
+    getStatus()
+    window.setInterval(getStatus, 20000)
+  },
 
   beforeModel() {
     this.i18n.i18next.use(LngDetector);
@@ -21,23 +52,6 @@ export default Route.extend(I18nMixin, {
       stimulation_voltage: 0,
       error: ''
     });
-  },
-
-  afterModel() {
-    this.store.findRecord('device', 1).then((device) => {
-      const batteryMessage = {message_type: 'get', message: 'battery'}
-      const batteryStatus = this.zmq.request(JSON.stringify(batteryMessage))
-      batteryStatus.then((response) => {
-        device.set('battery', response['payload']['battery_level']/100)
-      })
-
-      const stimMessage = {message_type: 'get', message: 'stim-status'}
-      const stimStatus = this.zmq.request(JSON.stringify(stimMessage))
-      stimStatus.then((response) => {
-        device.set('stimulation_voltage', response['payload']['stim_on'])
-      })
-    })
-
   },
 
   setupController(controller) {
