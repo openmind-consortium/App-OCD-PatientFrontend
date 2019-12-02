9e5de3ccf19450995c1fee22acc6d4114f324a76
fernandogelin <fernando@fernandogelin.com>
2019-12-02 14:07:39 -0500

    fix: import service in quit button

--- app/components/quit-button.js
+++ app/components/quit-button.js
@@ -1,5 +1,6 @@
 /* global require */
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
   zmq: service('zmq-client'),
