03de0e87dfed179b5154ab48e6fd0424de9a0538
Fernando Gelin <fernando@fernandogelin.com>
2020-03-25 15:09:44 -0400

    refactor: address requested changes

--- app/components/launch-task.js
+++ app/components/launch-task.js
@@ -5,14 +5,18 @@ export default Component.extend({
     actions: {
         launchTasks(taskName) {
           let taskPath;
-          if (taskName === 'beads') {
-            taskPath = 'beads\\beads.exe'
-          }
-          if (taskName === 'resting') {
-            taskPath = 'resting_state\\resting-state.exe'
-          }
-          if (taskName === 'msit') {
-            taskPath = 'task_msit\\task-msit.exe'
+          switch(taskName) {
+            case 'beads':
+              taskPath = 'beads\\beads.exe'
+              break;
+            case 'resting':
+              taskPath = 'resting_state\\resting-state.exe'
+              break;
+            case 'msit':
+              taskPath = 'task_msit\\task-msit.exe'
+              break;
+            default:
+              console.log('Wrong task name selected.')
           }
           ipcRenderer.send('taskSpawn', {taskPath})
         }
