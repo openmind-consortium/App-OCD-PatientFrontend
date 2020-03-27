c177e6d6d7076ee4994c0bd9c5a0804e23a419ff
Fernando Gelin <fernando@fernandogelin.com>
2020-03-27 08:59:05 -0400

    fix: disable double click for tasks

--- app/components/launch-task.js
+++ app/components/launch-task.js
@@ -1,7 +1,9 @@
 import Component from '@ember/component';
 const {ipcRenderer} = window.requireNode('electron');
+import { later } from '@ember/runloop';
 
 export default Component.extend({
+    launched: false,
     actions: {
         launchTasks(taskName) {
           let taskPath;
@@ -19,6 +21,10 @@ export default Component.extend({
               console.log('Wrong task name selected.')
           }
           ipcRenderer.send('taskSpawn', {taskPath})
+          this.set('launched', true)
+          later(this, function() {
+            this.set('launched', false)
+          }, 3000);
         }
       }
 });
